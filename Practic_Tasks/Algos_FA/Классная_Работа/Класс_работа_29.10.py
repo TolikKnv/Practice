3a414849180fc303e19f1cd7e4ee9fd7b035cba8
Konovalov Anatoliy <anatoliykonovalov2008@gmail.com>
2025-11-05 09:58:23 +0300

    Классная работа за 05.11.2025

--- Practic_Tasks/Algos_FA/Классная_Работа/Класс_работа_29.10.py
+++ Practic_Tasks/Algos_FA/Классная_Работа/Класс_работа_29.10.py
@@ -41,56 +41,56 @@
 
 
 # 7
-# letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-# chars = ["-.", ".-", "..", ".-.", "..-", "--.", ".--", ".-.-", "-.-.", "..--"]
-# d = {}
-# for i in range(len(letters)):
-#     d[chars[i]] = letters[i]
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+chars = ["-.", ".-", "..", ".-.", "..-", "--.", ".--", ".-.-", "-.-.", "..--"]
+d = {}
+for i in range(len(letters)):
+    d[chars[i]] = letters[i]
 
-# a = input()
-# words=[]
-# words = a.split('   ')
-# morze = []
-# for i in words:
-#     i = i.split(' ')
-#     morze.append(i)
-#     print(i)
+a = input()
+words=[]
+words = a.split('   ')
+morze = []
+for i in words:
+    i = i.split(' ')
+    morze.append(i)
+    print(i)
 
-# for i in morze:
-#     for j in range(len(i)):
-#         i[j]=d[i[j]]
+for i in morze:
+    for j in range(len(i)):
+        i[j]=d[i[j]]
 
-# for i in range(len(morze)):
-#     morze[i]=''.join(morze[i])
+for i in range(len(morze)):
+    morze[i]=''.join(morze[i])
 
-# print(*morze,sep = ' ')
+print(*morze,sep = ' ')
 
 
 # 9
-phones_list = [
-    {"name": "Ivan", "city": "Moscow", "phones": ["232-19-55", "+7 (916) 230-00-75"]},
-    {"name": "Anna", "city": "Samara", "phones": ["200-11-15"]},
-    {"name": "Anna", "city": "Vologda", "phones": ["+7 (931) 711-00-75"]},
-    {
-        "name": "Nikolay",
-        "city": "Moscow",
-        "phones": ["+7 (916) 778-71-05", "331-66-11", "783-33-85"],
-    },
-    {"name": "Ivan", "city": "Moscow", "phones": ["+7 (916) 205-41-05", "232-19-55"]},
-    {"name": "Anna", "city": "Samara", "phones": ["+7 (916) 105-13-56"]},
-]
-p_l = phones_list.copy()
-for i in range(len(phones_list)-1):
-    for j in range(i+1, len(phones_list)):
-        if phones_list[i]['name']==phones_list[j]['name'] and phones_list[i]['city']==phones_list[j]['city']:
-            for q in phones_list[j]['phones']:
-                if q in phones_list[i]['phones']:
-                    continue
-                else:
-                    phones_list[i]['phones'].append(q)
-                    p_l[i]['phones'].append(q)
-            p_l.pop(i)
+# phones_list = [
+#     {"name": "Ivan", "city": "Moscow", "phones": ["232-19-55", "+7 (916) 230-00-75"]},
+#     {"name": "Anna", "city": "Samara", "phones": ["200-11-15"]},
+#     {"name": "Anna", "city": "Vologda", "phones": ["+7 (931) 711-00-75"]},
+#     {
+#         "name": "Nikolay",
+#         "city": "Moscow",
+#         "phones": ["+7 (916) 778-71-05", "331-66-11", "783-33-85"],
+#     },
+#     {"name": "Ivan", "city": "Moscow", "phones": ["+7 (916) 205-41-05", "232-19-55"]},
+#     {"name": "Anna", "city": "Samara", "phones": ["+7 (916) 105-13-56"]},
+# ]
+# p_l = phones_list.copy()
+# for i in range(len(phones_list)-1):
+#     for j in range(i+1, len(phones_list)):
+#         if phones_list[i]['name']==phones_list[j]['name'] and phones_list[i]['city']==phones_list[j]['city']:
+#             for q in phones_list[j]['phones']:
+#                 if q in phones_list[i]['phones']:
+#                     continue
+#                 else:
+#                     phones_list[i]['phones'].append(q)
+#                     p_l[i]['phones'].append(q)
+#             p_l.pop(i)
 
-print(*phones_list,sep = '\n')
-print()
-print(*p_l,sep = '\n')
+# print(*phones_list,sep = '\n')
+# print()
+# print(*p_l,sep = '\n')
---
+++ Practic_Tasks/Algos_FA/Классная_Работа/Классная_работа_05.11.2025.py
@@ -0,0 +1,175 @@
+# 1
+# numbers = {
+#     'пятьдесят':50,
+#     'шестьдесят':60,
+#     'пять':5,
+#     'шесть':6,
+# }
+
+# numbers_int={}
+# for k,v in numbers.items():
+#     numbers_int[v]=k
+
+# # print(numbers_int)
+
+# var = int(input())
+# sum=[]
+# if var>9:
+#     sum.append(str(numbers_int[var//10*10]))
+# sum.append(str(numbers_int[var%10]))
+# print(' '.join(sum))
+
+
+# 2
+# letters = ["T", "O", "L", "I", "K"]
+# chars = ["-.", ".-", "..", ".-.", "..-"]
+# d = {}
+# for i in range(len(letters)):
+#     d[chars[i]] = letters[i]
+
+# d_in={}
+# for i in range(len(letters)):
+#     d_in[letters[i]] = chars[i]
+
+
+# def from_morze(a):
+#     words=[]
+#     words = a.split('   ')
+#     morze = []
+#     for i in words:
+#         i = i.split(' ')
+#         morze.append(i)
+#         print(i)
+
+#     for i in morze:
+#         for j in range(len(i)):
+#             i[j]=d[i[j]]
+
+#     for i in range(len(morze)):
+#         morze[i]=''.join(morze[i])
+
+#     print(*morze,sep = ' ')
+
+
+# def in_morze(b):
+#     words=[]
+#     words = b.split('   ')
+#     morze = []
+#     for i in words:
+#         i = i.split(' ')
+#         morze.append(i)
+#         print(i)
+
+#     for i in morze:
+#         for j in range(len(i)):
+#             i[j]=d_in[i[j]]
+
+#     for i in range(len(morze)):
+#         morze[i]=' '.join(morze[i])
+#     print(*morze,sep = ' ')
+
+# in_morze(input())
+
+
+# 7
+# d = [(i,i**2) for i in range(1,16)]
+# d_1 = {}
+# d_1.update(d)
+# print(d_1)
+
+
+# 8
+# d = {1: "q", 2: "w"}
+# d_1 = {3:'e',4:'r'}
+# d.update(d_1)
+# print(d)
+
+
+# 9
+# dict = {1: "q", 2: "w", 3: "e", 4: "r", 5: "t"}
+# for k,v in dict.items():
+#     print(k, ' = ', v)
+
+# 10
+# dict = {1: 1, 2: 2, 3: 3, 4: 4, 5: 4}
+# sum=0
+# for v in dict.values():
+#     sum+=v
+
+# print(sum)
+
+
+# 11
+# dict = {1: 1, 2: 2, 3: 3, 4: 4, 5: 4}
+# product = 1
+# for v in dict.values():
+#     product *= v
+
+# print(product)
+
+
+
+#12
+# key = int(input())
+# dict = {1: 1, 2: 2, 3: 3, 4: 4, 5: 4}
+# del dict[key]
+# print(dict)
+
+
+#13
+# list_1 = [1,2,3,4,5]
+# list_2 = [1,2,3,4,5]
+# list_con = [(list_1[i],list_2[i]) for i in range(len(list_1))]
+# d = {}
+# d.update(list_con)
+# print(d)
+
+
+
+#22
+# dict_ = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5}
+# dict = dict_.copy()
+# a = int(input())
+# for k,v in dict.items():
+#     if dict_[k]<a:
+#         del dict_[k]
+
+# print(dict_)
+
+#23
+# dict_ = {1: '1', 2: '11', 3: '111', 4: '1111', 5: '11111'}
+# max_key = None
+# max_value = -100000000000000000000
+# for k,v in dict_.items():
+#     if len(v)>max_value:
+#         max_key = k
+
+# print(max_key)
+
+
+
+#24
+# dict_ = {1: '1', 2: '11', 10: '111', 4: '1111', 5: '11111'}
+
+# dict_1 = {}
+
+# list_ = [(k,v) for k,v in dict_.items()]
+# list_ = list_[::-1]
+# dict_1.update(list_)
+# print(dict_1)
+
+
+
+# dict_2 = dict(sorted(dict_.items(), reverse=True))
+# print(dict_2)
+
+
+#25
+letter = input()
+dict_ = {'1': '1', '2': '11', '10': '111', '4': '1111', '5': '11111'}
+dict_1 = dict_.copy()
+for k in dict_.keys():
+    if k[0]==letter:
+        del dict_1[k]
+
+print(dict_1)
